da4490bc2c209b2845a87da33d25966aeee6f505
imajinyun <imajinyun@gmail.com>
2021-05-08 08:29:48 +0800

    Add binary-prefix-divisible-by-5 solution

---
+++ src/leetcode/BinaryPrefixDivisibleByFive.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace leetcode;
+
+class BinaryPrefixDivisibleByFive
+{
+    public static function prefixesDivByFive(array $arr): array
+    {
+        if (empty($arr)) {
+            return [];
+        }
+        [$ans, $num] = [[], 0];
+        foreach ($arr as $val) {
+            $num = ($num * 2 + $val) % 5;
+            $ans[] = $num === 0;
+        }
+
+        return $ans;
+    }
+
+    public static function prefixesDivByFive2(array $arr): array
+    {
+        if (empty($arr)) {
+            return [];
+        }
+        $num = 0;
+        foreach ($arr as $key => $val) {
+            $num = $num * 2 % 5 + $val;
+            $arr[$key] = $num % 5 === 0;
+        }
+
+        return $arr;
+    }
+}
---
+++ tests/leetcode/BinaryPrefixDivisibleByFiveTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace leetcode\tests;
+
+use PHPUnit\Framework\TestCase;
+use leetcode\BinaryPrefixDivisibleByFive;
+
+class BinaryPrefixDivisibleByFiveTest extends TestCase
+{
+    public function testPrefixesDivByFive(): void
+    {
+        self::assertSame(
+            [true, false, false],
+            BinaryPrefixDivisibleByFive::prefixesDivByFive([0, 1, 1])
+        );
+        self::assertSame(
+            [false, false, false],
+            BinaryPrefixDivisibleByFive::prefixesDivByFive([1, 1, 1])
+        );
+        self::assertSame(
+            [true, false, false, false, true, false],
+            BinaryPrefixDivisibleByFive::prefixesDivByFive([0, 1, 1, 1, 1, 1])
+        );
+        self::assertSame(
+            [false, false, false, false, false],
+            BinaryPrefixDivisibleByFive::prefixesDivByFive([1, 1, 1, 0, 1])
+        );
+    }
+
+    public function testPrefixesDivByFive2(): void
+    {
+        self::assertSame(
+            [true, false, false],
+            BinaryPrefixDivisibleByFive::prefixesDivByFive2([0, 1, 1])
+        );
+        self::assertSame(
+            [false, false, false],
+            BinaryPrefixDivisibleByFive::prefixesDivByFive2([1, 1, 1])
+        );
+        self::assertSame(
+            [true, false, false, false, true, false],
+            BinaryPrefixDivisibleByFive::prefixesDivByFive2([0, 1, 1, 1, 1, 1])
+        );
+        self::assertSame(
+            [false, false, false, false, false],
+            BinaryPrefixDivisibleByFive::prefixesDivByFive2([1, 1, 1, 0, 1])
+        );
+    }
+}
